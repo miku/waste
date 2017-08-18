da808da55e8ce09681a901524a239264f41b3f7b
Martin Czygan <martin.czygan@gmail.com>
2017-08-18 17:14:57 +0200

    add flag

--- cmd/waste/main.go
+++ cmd/waste/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"flag"
 	"io"
 	"log"
 	"net/http"
 )
 
+var listen = flag.String("l", "localhost:3000", "hostport")
+
 func main() {
+	flag.Parse()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "Hello World\n")
 	})
-	log.Fatal(http.ListenAndServe("localhost:3000", nil))
+	log.Printf("listening on %s", *listen)
+	log.Fatal(http.ListenAndServe(*listen, nil))
 }
