1d4237668d15b0f90cbe656931471bc74792cb6e
Martin Czygan <martin.czygan@gmail.com>
2017-09-03 10:30:42 +0200

    move things out to docker

--- cmd/waste/main.go
+++ cmd/waste/main.go
@@ -1,75 +1,43 @@
 package main
 
 import (
-	"context"
+	"bytes"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/client"
+	"github.com/miku/waste"
+	log "github.com/sirupsen/logrus"
 )
 
 var listen = flag.String("listen", "localhost:3000", "hostport")
 
 func main() {
 	flag.Parse()
+
+	log.SetLevel(log.DebugLevel)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		ctx := context.Background()
-
-		log.Println("creating new docker client")
-		cli, err := client.NewEnvClient()
-		if err != nil {
-			http.Error(w, "cannot create docker client", http.StatusInternalServerError)
-			return
+		var buf bytes.Buffer
+		wrapper := waste.WrapDocker{
+			ImageLocation: "docker.io/library/alpine",
+			ImageName:     "alpine",
+			Cmd:           []string{"uname", "-a"},
+			Writer:        &buf,
 		}
 
-		log.Println("pulling image")
-		_, err = cli.ImagePull(ctx, "docker.io/library/alpine", types.ImagePullOptions{})
-		if err != nil {
-			http.Error(w, "cannot pull image", http.StatusInternalServerError)
+		if err := wrapper.Run(); err != nil {
+			http.Error(w,
+				fmt.Sprintf("failed to run container: %s", err),
+				http.StatusInternalServerError)
 			return
 		}
-
-		log.Println("creating container")
-		resp, err := cli.ContainerCreate(ctx, &container.Config{
-			Image: "alpine",
-			Cmd:   []string{"uname", "-a"},
-		}, nil, nil, "")
-
-		if err != nil {
-			http.Error(w, "cannot create container", http.StatusInternalServerError)
-			return
-		}
-
-		log.Printf("starting container: %s", resp.ID)
-		if err = cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
-			http.Error(w, "cannot start container", http.StatusInternalServerError)
-			return
-		}
-
-		log.Printf("waiting for container: %s", resp.ID)
-		if _, err = cli.ContainerWait(ctx, resp.ID); err != nil {
-			http.Error(w, "container wait failed", http.StatusInternalServerError)
-			return
-		}
-
-		out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
-		if err != nil {
-			http.Error(w, "cannot access logs", http.StatusInternalServerError)
-			return
-		}
-		if _, err := io.Copy(w, out); err != nil {
-			http.Error(w, "cannot write response", http.StatusInternalServerError)
-			return
-		}
-
-		log.Printf("removing container: %s", resp.ID)
-		if err := cli.ContainerRemove(ctx, resp.ID, types.ContainerRemoveOptions{}); err != nil {
-			http.Error(w, "cannot remove container", http.StatusInternalServerError)
-			return
+		if _, err := io.Copy(w, &buf); err != nil {
+			http.Error(w,
+				fmt.Sprintf("failed copy stream: %s", err),
+				http.StatusInternalServerError)
 		}
 	})
 	log.Printf("listening on %s", *listen)
---
+++ docker.go
@@ -0,0 +1,88 @@
+package waste
+
+import (
+	"bufio"
+	"context"
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/moby/moby/api/types"
+	"github.com/moby/moby/api/types/container"
+	"github.com/moby/moby/client"
+	log "github.com/sirupsen/logrus"
+)
+
+// WrapDocker wraps information we need to run the isolated process.
+type WrapDocker struct {
+	ImageLocation string
+	ImageName     string
+	Cmd           []string
+	Writer        io.Writer
+	Timeout       time.Duration
+}
+
+// Run runs docker and executes the command in the container.
+func (w WrapDocker) Run() error {
+	ctx := context.Background()
+	if w.Timeout > 0 {
+		log.Debug("running with a timeout of %v", w.Timeout)
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithTimeout(ctx, w.Timeout)
+		defer func() {
+			log.Debug("operation timed out")
+			cancel()
+		}()
+	}
+
+	log.Debug("creating new docker client")
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		return err
+	}
+
+	log.Debug("pulling image from %s", w.ImageLocation)
+	_, err = cli.ImagePull(ctx, w.ImageLocation, types.ImagePullOptions{})
+	if err != nil {
+		return err
+	}
+
+	log.Debug("creating container from %s", w.ImageName)
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: w.ImageName,
+		Cmd:   w.Cmd,
+	}, nil, nil, "")
+
+	if err != nil {
+		http.Error(w, "cannot create container", http.StatusInternalServerError)
+		return
+	}
+
+	log.Debug("starting container %s", resp.ID)
+	if err = cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		return err
+	}
+
+	defer func() {
+		log.Debug("removing container %s", resp.ID)
+		err = cli.ContainerRemove(ctx, resp.ID, types.ContainerRemoveOptions{})
+	}()
+
+	log.Debug("waiting for container %s", resp.ID)
+	if _, err = cli.ContainerWait(ctx, resp.ID); err != nil {
+		return err
+	}
+
+	reader, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+	n, err := io.Copy(w.Writer, reader)
+	log.Debug("%d bytes read from application", n)
+	if bw, ok := w.Writer.(bufio.Writer); ok {
+		bw.Flush()
+	}
+	return err
+}
